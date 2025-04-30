853fab2793fde1c1b8d5f40efff07afc9be14be7
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-30 12:25:34 +0400

    render the product list for testing

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,12 +1,15 @@
 import {getTranslations} from 'next-intl/server';
 import CategoryBar from '../components/category/CategoryBar';
+import ProductList from '../components/product/ProductList';
  
 export default async function HomePage() {
   const t = await getTranslations('HomePage');
   return (
     <div>
       <CategoryBar/>
-      <h1>{t('title')}</h1>   
+      <h1>{t('title')}</h1>  
+
+      <ProductList /> 
     </div>
 
   )
