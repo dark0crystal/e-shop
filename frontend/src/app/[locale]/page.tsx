2e74c2530524cac9d28678e02469d518116d944a
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-30 09:38:51 +0400

    import the category bar in index for testing

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,6 +1,13 @@
 import {getTranslations} from 'next-intl/server';
+import CategoryBar from '../components/category/CategoryBar';
  
 export default async function HomePage() {
   const t = await getTranslations('HomePage');
-  return <h1>{t('title')}</h1>;
+  return (
+    <div>
+      <CategoryBar/>
+      <h1>{t('title')}</h1>   
+    </div>
+
+  )
 }
