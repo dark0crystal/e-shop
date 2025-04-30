93ef9d3300b66bb12770314cc2438b5edf1e6596
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-30 13:22:40 +0400

    use the collection card and render it in page with info

---
+++ frontend/src/app/components/collections/CollectionCardList.tsx
@@ -0,0 +1,41 @@
+
+'use client'
+import CollectionCard from "./CollectionCard";
+import caseImage from "../../../../public/case1.jpeg";
+
+const collections = [
+  {
+    id: 1,
+    name: "Summer Collection",
+    image: caseImage,
+  },
+  {
+    id: 2,
+    name: "Winter Collection",
+    image: caseImage,
+  },
+  {
+    id: 3,
+    name: "Spring Collection",
+    image: caseImage,
+  },
+];
+
+export default function CollectionCardList() {
+  const handleShopNow = (collectionName: string) => {
+    alert(`Redirecting to ${collectionName}`);
+  };
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
+      {collections.map((collection) => (
+        <CollectionCard
+          key={collection.id}
+          name={collection.name}
+          image={collection.image}
+          onShopNow={() => handleShopNow(collection.name)}
+        />
+      ))}
+    </div>
+  );
+}
