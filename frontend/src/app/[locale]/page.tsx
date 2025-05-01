66f2645be0b323395dccfa14b3d68544c7cbb48a
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-01 09:06:16 +0400

    use small ad container

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,15 +1,22 @@
 import {getTranslations} from 'next-intl/server';
 import CategoryBar from '../components/category/CategoryBar';
 import ProductList from '../components/product/ProductList';
+import WideAd from '../components/manage-ads-cards/WideAd';
+// import SmallAd from '../components/manage-ads-cards/SmallAd';
+import SmallAdContainer from '../components/manage-ads-cards/SmallAdContainer';
+
  
 export default async function HomePage() {
   const t = await getTranslations('HomePage');
   return (
     <div>
+      <WideAd imageUrl='/gaseem.jpeg'/>
+      <SmallAdContainer/>
       <CategoryBar/>
       <h1>{t('title')}</h1>  
 
       <ProductList /> 
+
     </div>
 
   )
