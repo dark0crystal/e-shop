dd63a8bba7341c0514739f8b04d2c8a2c95cb8db
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-04 22:21:03 +0400

    design and test product page

--- frontend/src/app/[locale]/admin/products/page.tsx
+++ frontend/src/app/[locale]/admin/products/page.tsx
@@ -1,10 +1,25 @@
+import { Link } from "@/i18n/routing";
 import ProductCard from "../ui/cards/ProductCard";
+import { getLocale } from "next-intl/server";
 
-export default function Products(){
+export default async function Products(){
+
+    const locale = (await getLocale()).substring(0, 2);
     return(
         <div>
             <h1>Products</h1>
-            <ProductCard/>
+            <div>
+                <div>
+                    <Link href="/admin/products/new" locale={locale} className="">
+                        <h1>New Product</h1>
+                    </Link>
+                </div>
+                <div>
+                    <ProductCard/>
+                </div>
+            </div>
+            
+           
         </div>
     )
 }
