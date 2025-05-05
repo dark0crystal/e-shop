55b13c10ef8677f982732acad4a8bb7310dac4ca
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-05 14:40:14 +0400

    design the products page

--- frontend/src/app/[locale]/admin/products/page.tsx
+++ frontend/src/app/[locale]/admin/products/page.tsx
@@ -1,25 +1,27 @@
 import { Link } from "@/i18n/routing";
 import ProductCard from "../ui/cards/ProductCard";
 import { getLocale } from "next-intl/server";
+import { Plus } from "lucide-react";
 
-export default async function Products(){
-
+export default async function Products() {
     const locale = (await getLocale()).substring(0, 2);
-    return(
+
+    return (
         <div>
             <h1>Products</h1>
             <div>
                 <div>
-                    <Link href="/admin/products/new" locale={locale} className="">
-                        <h1>New Product</h1>
+                    <Link href="/admin/products/new" locale={locale}>
+                        <h1 className="bg-blue-500 p-3 rounded-3xl w-fit text-white font-bold flex items-center gap-2">
+                            <Plus size={20} />
+                            New Product
+                        </h1>
                     </Link>
                 </div>
                 <div>
-                    <ProductCard/>
+                    <ProductCard />
                 </div>
             </div>
-            
-           
         </div>
-    )
+    );
 }
