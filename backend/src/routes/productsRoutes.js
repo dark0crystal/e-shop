ee9b51ced7839226c223d3a7539b2a5f4d2836ab
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-08 09:59:55 +0400

    test new product insertion

--- backend/src/routes/productsRoutes.js
+++ backend/src/routes/productsRoutes.js
@@ -5,15 +5,33 @@ const router = express.Router();
 
 
 router.post('/add-new-post', async (req, res) => {
-    try{
-        const {name , description,brand , isActive ,categoryId ,price, sku} = req.body;
-        console.log("the name is :",name)
-
-    }catch(error){
-        console.log(error)
+    try {
+      const { name, description, brand, isActive,  price, sku } = req.body;
+  
+     
+  
+      const product = await prisma.product.create({
+        data: {
+          name,
+          description,
+          brand,
+          isActive,
+          categoryId:"6a1a4696-dcbb-4f3f-9f46-d9b6b7c427a6",
+          coverImage: "fdasfadfmlasdfm",
+        },
+      });
+      console.log()
+      res.status(201).json({
+        message: "Product created successfully",
+        product,
+      });
+  
+    } catch (error) {
+      console.error("Error creating product:", error);
+      res.status(500).json({ message: "Internal server error", error });
     }
-
-})
+  });
+  
 
 export default router;
 
