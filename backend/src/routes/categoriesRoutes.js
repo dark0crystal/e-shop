864fa57d625a07b50b10fd2e400096dd016d9a33
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-11 13:21:40 +0400

    fix typo

--- backend/src/routes/categoriesRoutes.js
+++ backend/src/routes/categoriesRoutes.js
@@ -49,17 +49,17 @@ router.get('/all', async(req, res)=>{
 //====================================================
 // get the parent categories only 
 
-router.get('get-parent-categories' , async(req ,res)=>{
+router.get('/get-parent-categories' , async(req ,res)=>{
   try{  
     const parentCategories = await prisma.categories.findMany({
         where:{
           parentCategoryId :null
         }
     })
-
+    console.log(parentCategories)
     res.status(200).json(parentCategories)
   }catch(error){
-    res.status(404).message({message:"can't get the parent categoreis"});
+    res.status(404).json({message:"can't get the parent categoreis"});
   }
 })
 //====================================================
