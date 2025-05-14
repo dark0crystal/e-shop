8ea54ef361bc29cdc135b5466800d2b473b4d954
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-14 21:59:11 +0400

    checkout page

---
+++ frontend/src/app/[locale]/(main)/checkout/page.tsx
@@ -0,0 +1,14 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { z } from 'zod';
+export default function Checkout(){
+
+    
+
+    return(
+        <div>
+            <form>
+
+            </form>
+        </div>
+    )
+}
