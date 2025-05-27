34271beab3a35ac1e60e76188d71ed896a58e8ac
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-27 21:28:57 +0400

    seperate the checkout orders display

---
+++ frontend/src/app/[locale]/(main)/checkout/CheckoutOrders.tsx
@@ -0,0 +1,31 @@
+
+/**
+ * Fetch orders for the cart and display them in nice designed card.
+ */
+export default async function CheckOutOrders(){
+
+
+  return(
+    <div>
+      Checkout
+    </div>
+  )
+
+}
+
+
+/**
+ * Calculate the total price
+ */
+
+function CalculateTotalPrice(){
+    return{}
+}
+
+/**
+ * Apply discount
+ */
+
+function ApplyDiscount(){
+    return{}
+}
