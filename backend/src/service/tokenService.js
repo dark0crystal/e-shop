a090a1f912407ec2d038f5f003f4c05431c17ccc
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-28 11:24:40 +0400

    remove

--- backend/src/service/tokenService.js
+++ backend/src/service/tokenService.js
@@ -1,10 +1,9 @@
-// src/service/tokenService.js
 import jwt from 'jsonwebtoken';
 
 const JWT_PRIVATE_KEY = process.env.JWT_SECRET;
 
 export function generateAuthToken(userId, isAdmin) {
-  return jwt.sign({ userId, isAdmin }, JWT_PRIVATE_KEY);
+  return jwt.sign({ userId, isAdmin }, JWT_PRIVATE_KEY, { expiresIn: '24h' });
 }
 
 export function decodeToken(token) {
