1fefeec426ea6acfe72fd8e3834995b6ba96d587
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-03 16:11:50 +0400

    get the session id and redirect the user to thawani custom page

--- frontend/src/app/[locale]/(main)/checkout/CheckoutForm.tsx
+++ frontend/src/app/[locale]/(main)/checkout/CheckoutForm.tsx
@@ -94,10 +94,13 @@ export default function CheckOutForm() {
           paymentMethodId,
         }),
       });
+      const data = await checkoutResponse.json();
+      const session_id = data.session_id;
+      console.log("session_id", session_id);
+      const checkoutUrl = `https://uatcheckout.thawani.om/pay/${session_id}?key=${process.env.THAWANI_PUBLISHABLE_KEY}`
 
+      console.log("checkoutUrl", checkoutUrl);
       if (!checkoutResponse.ok) throw new Error('Failed to create checkout session');
-
-      const { checkoutUrl, orderId } = await checkoutResponse.json();
 
       // Redirect to Thawani checkout page
       window.location.href = checkoutUrl;
