7a1b68d31fccd2aa671dd23220938c35ccd975aa
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-04 16:52:29 +0400

    convert wideAd to client component

--- frontend/src/app/components/manage-ads-cards/WideAd.tsx
+++ frontend/src/app/components/manage-ads-cards/WideAd.tsx
@@ -1,12 +1,28 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default async function WideAd({ imageUrl, link }: { imageUrl: string; link?: string }) {
-  const response = await fetch("http://localhost:8383/ads/get-ads/wide");
-  const wideAdImageUrl = await response.json();
+export default function WideAd({ imageUrl, link }: { imageUrl: string; link?: string }) {
+  const [wideAdImageUrl, setWideAdImageUrl] = useState<string>("");
+
+  useEffect(() => {
+    const fetchAdImage = async () => {
+      try {
+        const response = await fetch("http://localhost:8383/ads/get-ads/wide");
+        const data = await response.json();
+        setWideAdImageUrl(data);
+      } catch (error) {
+        console.error("Error fetching ad image:", error);
+      }
+    };
+
+    fetchAdImage();
+  }, []);
 
   const adImage = (
-    <div className="relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden  shadow-md">
+    <div className="relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden shadow-md">
       <Image
         src={wideAdImageUrl}
         alt="Wide Ad Banner"
