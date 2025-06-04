1292856d67859c2d452ae63443492fdfaac89a1d
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-04 16:47:36 +0400

    fix

--- frontend/src/app/components/manage-ads-cards/WideAd.tsx
+++ frontend/src/app/components/manage-ads-cards/WideAd.tsx
@@ -1,12 +1,14 @@
-// components/WideAd.tsx
 import Image from "next/image";
 import Link from "next/link";
 
-export default function WideAd({ imageUrl, link }: { imageUrl: string; link?: string }) {
+export default async function WideAd({ imageUrl, link }: { imageUrl: string; link?: string }) {
+  const response = await fetch("http://localhost:8383/ads/get-ads/wide");
+  const wideAdImageUrl = await response.json();
+
   const adImage = (
     <div className="relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden  shadow-md">
       <Image
-        src={imageUrl}
+        src={wideAdImageUrl}
         alt="Wide Ad Banner"
         fill
         className="object-cover"
