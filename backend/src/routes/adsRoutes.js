428626ed01db57d0c72ccc7bbcc904194f4c43dc
ALMardasDEV <albusaidi9094@gmail.com>
2025-06-04 16:46:30 +0400

    create new api endpoints

--- backend/src/routes/adsRoutes.js
+++ backend/src/routes/adsRoutes.js
@@ -6,18 +6,50 @@ import { verifyToken } from "./authRoutes.js";
 
 const router = express.Router();
 
+//---------------------------------
+
 // should add caching here
-router.get('/get-ads', async (req, res) => {
+router.get('/get-ads/wide', async (req, res) => {
     try {
         const ads = await prisma.ad.findFirst({
             where: {
-                type: req.params.type
+                type: "wide"
+            },
+            select: {
+                image_url: true
             }
         });
-        res.status(200).json(ads);
+        res.status(200).json(ads.image_url);
     } catch (error) {
         res.status(500).json({ message: "Failed to get ads" });
     }
 });
+
+//---------------------------------
+// create ad
+
+router.post('/create-ad', async (req, res) => {
+    try {
+        console.log("req.body", req.body);
+        const { image_url, type } = req.body;
+        // store the image url and type in the database
+        const ad = await prisma.ad.create({
+            data: {
+                image_url: image_url,
+                type: type
+            }
+        });
+        res.status(200).json(ad);
+    } catch (error) {
+        console.error("Error creating ad:", error);
+        res.status(500).json({ message: "Failed to create ad" });
+    }
+});
+
+
+
+//---------------------------------
+
+
 
 export default router;
